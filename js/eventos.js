3e2c39bfe84ba8e5f4a08ef9409353afab44f98b
MelianGithub <e_melian04@hotmail.com>
2023-08-09 10:42:27 -0300

    Arreglos en solucion-click

--- js/eventos.js
+++ js/eventos.js
@@ -1,4 +1,12 @@
 const div = document.getElementById("div")
-div.addEventListener('click', () => {
-alert('Hola, soy un div')
+div.addEventListener('click', (e) => {
+    alert('Hola, soy un div');
+
 })
+
+const saludar = document.getElementById("saludar")
+saludar.addEventListener('click', (e) => {
+    e.stopPropagation();
+    alert('Hola!');
+})
+
